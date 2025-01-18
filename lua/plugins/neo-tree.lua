910f1ee0309545bdfa0cbb802b7656c28a2d75d3
Piotr Borek <piotrborek@op.pl>
2025-01-18 17:51:59 +0100

    Defined hiden files.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,4 +7,18 @@ return {
     "MunifTanjim/nui.nvim",
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = false,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_hidden = false,
+        hide_by_name = { },
+        never_show = {
+          ".git"
+        }
+      }
+    }
+  }
 }
